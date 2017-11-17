4a3f56d6aab6e547fe62e2fabe07f44efcb5e575
HarmanU <uppalhrs@aston.ac.uk>
2017-11-17 11:42:04 +0000

    Added Tool for reading CSV's

--- src/com/aston/aName/Util/CSVTool.java
+++ src/com/aston/aName/Util/CSVTool.java
@@ -1,46 +1,85 @@
-package Util;
+package com.aston.aName.Util;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class CSVTool {
 	
-	public CSVTool()
+	private static String csvFilePath = "./MTRsystem_partial.csv"; // Default will look for file in root
+	private static BufferedReader br = null;
+	private static ArrayList<String> lastFileReadStorage;
+	
+	public static ArrayList<String> CSVToStringList()
 	{
-		String csvFile = "./file/example.csv";
-        BufferedReader br = null;
-        String line = "";
-        String cvsSplitBy = ",";
+
+		String line = "";
+		ArrayList<String> lineStorage = new ArrayList<String>();
 
         try {
 
-            br = new BufferedReader(new FileReader(csvFile));
+            br = new BufferedReader(new FileReader(csvFilePath));
             while ((line = br.readLine()) != null) {
+            
+            lineStorage.add(line);
 
-                // use comma as separator
-                String[] lineArray = line.split(cvsSplitBy);
-                
-                System.out.println(line);
-
-                System.out.println("Line [ID = " + lineArray[0] + " , Type = " + lineArray[1] + " , Name = " + lineArray[2] + "]");
+//               //use comma as separator
+//                String[] lineArray = line.split(SplitterDelimiter);
+//                
+//                System.out.println(line);
+//
+//                System.out.println("Line [ID = " + lineArray[0] + " , Type = " + lineArray[1] + " , Name = " + lineArray[2] + "]");
 
             }
 
-        } catch (FileNotFoundException e) {
+        } 
+        catch (FileNotFoundException e) 
+        {
             e.printStackTrace();
-        } catch (IOException e) {
+            System.out.println("No file was found at that location. File path should look similar to this ( ./file.csv)");
+        } 
+        catch (IOException e) 
+        {
             e.printStackTrace();
-        } finally {
-            if (br != null) {
-                try {
+        } 
+        finally 
+        {
+            if (br != null) 
+            {
+                try 
+                {
                     br.close();
-                } catch (IOException e) {
+                } 
+                catch (IOException e) 
+                {
                     e.printStackTrace();
                 }
             }
         }
+        
+        System.out.println("Load Successful");
+        
+        lastFileReadStorage = lineStorage;
+        
+        return lineStorage;
+		
 	}
 	
-
+	public static String getCurrentPath()
+	{
+		return csvFilePath;
+	}
+	
+	public static void setFilePath(String newPath)
+	{
+		csvFilePath = newPath;
+	}
+	
+	public static ArrayList<String> getLastReadFile()
+	{
+		return lastFileReadStorage;
+	}
+	
+        
 }
