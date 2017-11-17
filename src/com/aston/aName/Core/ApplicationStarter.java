efeafd0cd030f9731824357054a71fb80e07cc00
miahms <miahms@aston.ac.uk>
2017-11-17 12:20:36 +0000

    added ui to main and scanner

--- src/com/aston/aName/Core/ApplicationStarter.java
+++ src/com/aston/aName/Core/ApplicationStarter.java
@@ -1,12 +1,17 @@
 package com.aston.aName.Core;
 
+import java.util.Scanner;
+
 import com.aston.aName.Util.CSVTool;
+import com.aston.aName.View.TUI;
 
 public class ApplicationStarter {
-
+	private static Scanner scan = new Scanner(System.in);
+	
 	public static void main(String[] args) {
-		
 		CSVTool.CSVToStringList();
+		TUI tui = new TUI(scan);
+		tui.textUserInterface();
 
 	}
 
