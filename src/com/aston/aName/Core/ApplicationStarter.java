72aca2a67dc113558b9cdfbaa7ff336bac9ff471
HarmanU <uppalhrs@aston.ac.uk>
2017-11-17 11:42:18 +0000

    Will start CSV Reader for testing

---
+++ src/com/aston/aName/Core/ApplicationStarter.java
@@ -0,0 +1,13 @@
+package com.aston.aName.Core;
+
+import com.aston.aName.Util.CSVTool;
+
+public class ApplicationStarter {
+
+	public static void main(String[] args) {
+		
+		CSVTool.CSVToStringList();
+
+	}
+
+}
