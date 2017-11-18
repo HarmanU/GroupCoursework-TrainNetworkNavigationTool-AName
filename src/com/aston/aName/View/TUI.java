ccdbecf24d3027bba7735ca57d222729c5e84ca2
miahms <miahms@aston.ac.uk>
2017-11-18 12:29:21 +0000

    Added comments, made improvements to UI.

--- src/com/aston/aName/View/TUI.java
+++ src/com/aston/aName/View/TUI.java
@@ -5,55 +5,39 @@ import com.aston.aName.Util.CSVTool;
 
 public class TUI {
 	private Scanner reader;
-	
-	
+
 	public TUI(Scanner reader) {
 		this.reader  =reader;
 	}
-	
-	//ability to change path for file
-	
+
+	/**
+	 * Creates UI which receives input from user.
+	 */
 	public void textUserInterface(){
-		boolean run = true;
 		System.out.println("Hello and welcome to MTR \n");
 		commands();
-		int response = Integer.parseInt(reader.nextLine());
-		
-		while(run) {
-		
-		switch (response) {
-		
-		case 1: System.out.println("first selected");
-				run = false;
-				break;
-		case 2: System.out.println("second selected");
-				run = false;
-				break;
-		case 3: System.out.println("third selected");
-				run = false;
-				break;
-		case 4: System.out.println("fourth selected");	
-				run = false;
-				break;
-		case 5: System.out.println("Your current path is: " + CSVTool.getCurrentPath());	
-				System.out.println("Specify new path: ");
-				String path = reader.nextLine();
-				CSVTool.setFilePath(path);	
-				run = false;
-				break;
-		case 6: run = false;
-				break;		
-		default: System.out.println("Invalid option");
-				commands();
-				run = false;
-				break;
+
+		while(true) {
+			int response = 0;
+			//if user enters string, prompts user to enter a number.
+			try{
+				response = Integer.parseInt(reader.nextLine());
+				if(response == 6) { //exits loop
+					break;
+				}
+				loop(response);
+			}
+			catch(NumberFormatException e){
+				System.out.println("Please enter a number corresponding to an option \n \t For example pressing 6 will quit \n");
+			}
+			commands();
 		}
+		System.out.println("Goodbye!");	
 	}
-		
-	 System.out.println("Goodbye!");	
-	}
-	
-	public void commands() {
+	/**
+	 * Functions available to user
+	 */
+	private void commands() {
 		System.out.println("Following commands are available: ");
 		System.out.println("[1] Find terminal in common");
 		System.out.println("[2] Find path between two terminals");
@@ -61,6 +45,42 @@ public class TUI {
 		System.out.println("[4] List all terminals");
 		System.out.println("[5] Specify file path");
 		System.out.println("[6] Quit");
+		System.out.print("> ");//cursor (where user enters)
 	}
-	
+
+	/**
+	 * Receives decision from textUserInterface().
+	 * Then carries out function.
+	 * @param decision received from textUserInterface
+	 */
+	private void loop(int decision) {
+
+		switch (decision) {
+
+		case 1: System.out.println("first selected (Function null)");
+		break;
+		case 2: System.out.println("second selected (Function null)");
+		break;
+		case 3: System.out.println("third selected (Function null)");
+		break;
+		case 4: System.out.println("fourth selected (Function null)");	
+		break;
+		case 5: changeFilePath();
+		break;	
+		default: System.out.println("Invalid option \n");// if not options 1-6 user is told so
+		break;
+		}
+	}
+
+	/**
+	 * Changes the file path, to whatever the suer enters
+	 */
+	private void changeFilePath(){
+		System.out.println("The current file path is: " + CSVTool.getCurrentPath());	
+		System.out.print("Specify new path: ");
+		String path = reader.nextLine();//new path
+		CSVTool.setFilePath(path);
+		System.out.println("The file Path is now: " + CSVTool.getCurrentPath() + "\n");//shows new path
+	}
+
 }
