727abdc169c20e3f5e462d292552e1b3f4e008f5
HarmanU <uppalhrs@aston.ac.uk>
2017-11-18 00:37:27 +0000

    Added Timer, Added Debug Toggle, Cleaned code

--- src/com/aston/aName/Core/ApplicationStarter.java
+++ src/com/aston/aName/Core/ApplicationStarter.java
@@ -1,26 +1,38 @@
 package com.aston.aName.Core;
 
+import java.util.Date;
 import java.util.Scanner;
 
 import com.aston.aName.Util.CSVTool;
 import com.aston.aName.View.TUI;
 
 public class ApplicationStarter {
+	
 	private static Scanner scan = new Scanner(System.in);
 	
+	public static boolean DEBUG = true;
+	
 	public static void main(String[] args) {
-		CSVTool.CSVToStringList();
-		TUI tui = new TUI(scan);
-		tui.textUserInterface();
-
-		System.out.println(CSVTool.getCurrentPath());
 		
+		if (args != null)
+		{
+			DEBUG = Boolean.parseBoolean(args[0]);
+		}
+		
+		// Start Timer
+		long startTime = new Date().getTime();
+		
+		//System Start up
+		CoreSystem.generateMap(CSVTool.CSVToStringList(), DEBUG);
+		TUI tui = new TUI(scan);
+		
+		// End Timer
+		long endTime = new Date().getTime();
+		
+		System.out.println("Total Initialisation time: " + (endTime - startTime) + "ms");
 		System.out.println("");
 		
-		for(String s : CSVTool.getLastReadFile())
-		{
-			System.out.println(s);
-		}
+		tui.textUserInterface();
 	}
 
 }
