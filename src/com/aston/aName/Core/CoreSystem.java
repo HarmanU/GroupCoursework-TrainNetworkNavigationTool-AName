114b99d73c7649cb1b786aa1850ca44c59f4d95a
HarmanU <uppalhrs@aston.ac.uk>
2017-12-02 21:22:13 +0000

    Is now Controller, has updated methods
    Now implements controller, this will be the controller for our system.
    This will include the methods for TUI.
    
    Implemented No. 2 and 3 of the 4 methods.

--- src/com/aston/aName/Core/CoreSystem.java
+++ src/com/aston/aName/Core/CoreSystem.java
@@ -1,13 +1,20 @@
 package com.aston.aName.Core;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
-public class CoreSystem {
+import com.aston.aName.Util.Controller;
+import com.sun.org.apache.xml.internal.resolver.helpers.Debug;
+
+public class CoreSystem implements Controller{
 	
 	private static CoreSystem instance;
 	
 	private static ArrayList<Line> linesInSystem = new ArrayList<Line>();	
 	private static ArrayList<Station> stationsInSystem = new ArrayList<Station>();
+	
+	private static boolean DEBUG;
 	
 	//----------------------------------------------------- Ensures single Instance -----------------------------------------------------
 	private CoreSystem()
@@ -25,9 +32,14 @@ public class CoreSystem {
 		return instance;
 	}
 	
+	public static void setDebugState(boolean newDebugState)
+	{
+		DEBUG = newDebugState;
+	}
+	
 	//-----------------------------------------------------|-----------------------|-----------------------------------------------------
 	
-	public static void generateMap(ArrayList<String> CSVData, boolean debug)
+	public static void generateMap(ArrayList<String> CSVData)
 	{
 		StringBuilder log = new StringBuilder();
 		
@@ -38,7 +50,7 @@ public class CoreSystem {
 		createLinks(CSVData);
 		
 		// To Print Debug information if debug is set to true
-		if (debug)
+		if (DEBUG)
 		{
 			log.append("\n\nTotal of " + linesInSystem.size() + " Lines added");
 			log.append("\nTotal of " + stationsInSystem.size() + " Stations added");
@@ -99,7 +111,12 @@ public class CoreSystem {
 	{
 		for (Line l : linesInSystem)
 		{
-			l.refreshLines();
+			l.registerLine();
+		}
+		
+		for (Line l : linesInSystem)
+		{
+			l.registerConnectedLines();
 		}
 	}
 	
@@ -107,6 +124,153 @@ public class CoreSystem {
 		TerminiNode t = new TerminiNode(linesInSystem);
 	}
 
+	@Override
+	public String listAllTermini() {
+		
+		
+		
+		return null;
+	}
 
+	/*
+	 * Method will list all stations on a line
+	 * 'Should be O(N)'
+	 */
+	@Override
+	public String listStationsInLine(String line) {
+		
+		StringBuilder sb = new StringBuilder();
+		Line desiredLine = null;
+		
+		// Search for Line in System
+		for (Line l : linesInSystem)
+		{
+			if (l.equals(line))
+			{	
+				if (DEBUG)
+				{
+					sb.append("\nCurrent Line Name: " + l.getLineName());
+					sb.append("\nSearch Term: " + line);
+				}
+				desiredLine = l;
+				break;
+			}
+		}
+		
+		sb.append("\n--------------------------- Current Query ---------------------------");
+		
+		if (desiredLine != null)
+		{
+			// Append desiredLine toString
+			sb.append(desiredLine.toString());
+		}
+		else if (desiredLine == null)
+		{
+			// Append an error message if a line was not found
+			sb.append(printLineNotFound());
+		}
+		
+		// DEBUG CALL
+		if (DEBUG && desiredLine != null)
+		{
+			sb.append("\nLine Index called:" + linesInSystem.indexOf(desiredLine));
+		}
+
+		sb.append("\n---------------------------------------------------------------------");
+		return sb.toString();
+	}
+
+	/*
+	 * Method to list all connected lines on a line and at what station
+	 * 'Should be O(N)'
+	 */
+	@Override
+	public String listAllDirectlyConnectedLines(String line) 
+	{
+		StringBuilder sb = new StringBuilder();
+		Line desiredLine = null;
+		
+		// Search for Line in System
+				for (Line l : linesInSystem)
+				{
+					if (l.equals(line))
+					{	
+						if (DEBUG)
+						{
+							sb.append("\nCurrent Line Name: " + l.getLineName());
+							sb.append("\nSearch Term: " + line);
+						}
+						
+						desiredLine = l;
+						break;
+					}
+				}
+				
+				if (DEBUG && desiredLine != null)
+				{
+					sb.append("\nLine Index called:" + linesInSystem.indexOf(desiredLine));
+				}
+				
+				if(desiredLine != null && desiredLine.getConnectedLines().isEmpty() == false)
+				{
+					HashMap<Line, Station> connectedLines = desiredLine.getConnectedLines();
+					sb.append("\n--------------------------- Current Query ---------------------------");
+					sb.append("\nLines connected to this one include:");
+					
+					for (Map.Entry<Line, Station> entry : connectedLines.entrySet())
+					{
+					    sb.append("\nThe " + entry.getKey().getLineName());
+					}
+				}
+				else if (desiredLine != null && desiredLine.getConnectedLines().isEmpty() == true)
+				{
+					sb.append("\nNo Lines connect to " + desiredLine.getLineName());
+				}
+				else if (desiredLine == null)
+				{
+					printLineNotFound();
+				}
+				
+		sb.append("\n---------------------------------------------------------------------");
+		return sb.toString();
+	}
+
+	@Override
+	public String showPathBetween(String stationA, String stationB) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	/**
+	 * A method for printing an error message if a line was not found in a query
+	 * @return will return a String with error message
+	 */
+	public String printLineNotFound()
+	{
+		StringBuilder sb = new StringBuilder();
+		
+		// If no line was found then print message and list of current valid lines
+		sb.append("\nLine of that name not found, ensure you typed the line name correctly including correct case");
+		sb.append("\n\nCurrent lines include");
+		for (Line l : linesInSystem)
+		{
+			sb.append("\n" + l.getLineName());
+		}	
+		return sb.toString(); 
+	}
+
+	//--------------------------------------------------------- Getter Methods --------------------------------------------------------
+
+	public static ArrayList<Line> getLinesinSystem()
+	{
+		return linesInSystem;
+	}
+	
+	public static ArrayList<Station> getStationinSystem()
+	{
+		return stationsInSystem;
+	}
+		
+	//-----------------------------------------------------|-----------------------|-----------------------------------------------------
 	
 }
